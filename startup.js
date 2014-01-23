76f5ec51d704937e778106f904c0b378eccd8fea
William Scott Wright <cpress.scott@gmail.com>
2014-01-22 22:48:49 -0500

    Initial commit.
    Initial commit.

---
+++ startup.js
@@ -0,0 +1,177 @@
+var Game = [];
+var StartupName;
+Game.Money = 0.0;
+Game.VPSRatio = 0.001;
+Game.SEOAmount = 1000;
+Game.Power = 500;
+
+Game.UPGRADE_TYPES = {
+  POWER : {value: 0, name: "Power"}, 			//Upgrades to the maximum power usage
+  VPSRATIO: {value: 1, name: "VPS Ratio"}, 		//Upgrades to the VPS Ratio
+  SEOAMOUNT : {value: 2, name: "SEO Amount"}	//Upgrades to the SEO Amount
+};
+
+Game.Items = [];
+Game.Items[0] = {'Name':'Cheap-o Host', 'Price':4.99, 'VPS':30, 'Power':0, 'Description':'A really cheap online host. It boasts 9% uptime! Hardware specs unknown.'};
+Game.Items[1] = {'Name':'Raspberry Pi', 'Price':35, 'VPS':200, 'Power':10, 'Description':'A tiny PC on a single circuit board. I/O is incredibly slow, but it works. 700Mhz processor with 512MB of RAM.'};
+Game.Items[2] = {'Name':'Netbook', 'Price':100, 'VPS':450, 'Power':50, 'Description':'A cheap netbook with a 1Ghz processor and 1GB of RAM.'};
+Game.Items[3] = {'Name':'Refurbished Desktop', 'Price':200, 'VPS':700, 'Power':300, 'Description':'A refurbished consumer-grade desktop PC with a 2Ghz processor and 2GB of RAM.'};
+Game.Items[4] = {'Name':'Dedicated Server', 'Price':2000, 'VPS':10000, 'Power':800, 'Description':'A dedicated single-CPU server with a 2.8Ghz processor and 8GB of RAM.'};
+Game.Items[5] = {'Name':'Blade Server', 'Price':30000, 'VPS':1000000, 'Power':2000, 'Description':'A rack-mounted Blade Server with two 3.2Ghz processors and 32GB of RAM.'};
+Game.Items[6] = {'Name':'Amazon EC2', 'Price':250000, 'VPS':15000000, 'Power':15000, 'Description':'An Amazon EC2 instance with 4 virtual CPUs at 3.2Ghz and 64GB of RAM. Unfortunately, you have to pay the power bill too.'};
+
+Game.Upgrades = [];
+Game.Upgrades[0] = {'Name':'Improved Breaker Box', 'Price':250, 'Type': Game.UPGRADE_TYPES.POWER, 'Description':'Upgrades to maximum power usage by 500 watts.', 'PwrReq':250, 'MnyReq':0, 'VPSReq':100}
+
+Game.infrastructure = [];
+
+$(document).ready(function() {
+ 	RegisterHooks();
+ 	$('.mainContent').hide();
+ 	$('#startupNameHeader').hide();
+
+ 	//Create an infrastructure element for each possible infrastructure type
+ 	for(var i=0; i<Game.Items.length; i++) {
+ 		Game.infrastructure[i] = 0;
+ 	}
+ 	$('#lblRatio').text(Game.VPSRatio);
+
+ 	//Setup tooltip settings
+ 	$( document).tooltip({
+      track: true,
+      hide: { effect: "fadeOut", duration: 0 },
+      show: { effect: "fadeIn", duration: 0 }
+    });
+});
+
+//Registers all control events and hooks
+function RegisterHooks() {
+	$('#startupName-Form').on('submit', function(e) {
+		e.preventDefault();	//Prevent form from submitting
+		var data = $("#startupName-Form :input").serializeArray();
+		Game.StartupName = data[0].value;
+		StartGame();
+	});
+
+	$('#btnSEO').click(function(e) {
+		PerformSEO();
+	});
+
+
+}
+
+function PerformSEO() {
+	var amount = Game.SEOAmount * Game.VPSRatio;
+	AddMoney(amount);
+}
+
+//Starts the game
+function StartGame() {
+	//Hides the form used to name the startup
+	$('.nameStartupDiv').hide();
+	$('#startupNameHeader').text(Game.StartupName);
+	$('#startupNameHeader').show();
+	$('.mainContent').show();
+
+	CreatePurchaseEntries();
+	UpdatePower();
+	Run();
+}
+
+function Run() {
+   setTimeout(Run,100);
+   if(Game.infrastructure.length > 0) {
+   		var vps = CalculateVPS();
+   		var mps = vps * Game.VPSRatio;
+   		var tickMoney = mps * 0.1;
+   		AddMoney(tickMoney);
+   		$('#lblMoney').text(Game.Money.toFixed(2));
+   		$('#lblVPS').text(vps.toFixed(0));
+   		$('#lblMPS').text(mps.toFixed(2));
+   }
+}
+
+function CalculateVPS() {
+	var total = 0.0;
+	for(var i=0; i<Game.infrastructure.length; i++) {
+		total += Game.infrastructure[i] * Game.Items[i].VPS;
+	}
+	return total;
+}
+
+function CalculatePowerUsed() {
+	var total = 0.0;
+	for(var i=0; i<Game.infrastructure.length; i++) {
+		total += Game.infrastructure[i] * Game.Items[i].Power;
+	}
+	return total;
+}
+
+function CalculatePowerPercentage() {
+	var used = CalculatePowerUsed();
+	return used / Game.Power;
+}
+
+function AddMoney(i) {
+	Game.Money += i;
+}
+
+function RemoveMoney(i) {
+	Game.Money -= i;
+}
+
+//Creates all of the items available for purchase
+function CreatePurchaseEntries() {
+	var purchaseBox = $('#purchaseBox');
+	for(var i=0; i<Game.Items.length; i++) {
+		var appendText = "<div class='itemBox' title='"+CreateHardwareTooltip(Game.Items[i])+"'><label for='purchaseButton"+i+"'>" + Game.Items[i].Name + "</label><input type='Button' class='purchaseButton' id='purchaseButton"+i+"' value='$"+Game.Items[i].Price+"' /></div>";
+		$(purchaseBox).append(appendText);
+		RegisterPurchaseHook(i);
+	}
+}
+
+function CreateHardwareTooltip(item) {
+	//TODO: create this
+	return item.Description + " [Power:" + item.Power + "]";
+}
+
+function UpdateInfrastructure() {
+	var infrastructureBox = $('#infrastructureBox');
+	infrastructureBox.empty();
+	for(var i=0; i<Game.infrastructure.length; i++) {
+		if(Game.infrastructure[i] > 0) {
+			var appendText = "<div class='itemBox'><span class='infrastructureName'>" + Game.Items[i].Name + "</span><span class='infrastructureCount'>"+Game.infrastructure[i]+"</span></div>";
+			$(infrastructureBox).append(appendText);
+		}
+	}
+	UpdatePower();
+}
+
+function UpdatePower(pwr) {
+/*	//Remove the existing caption
+	$('#powerProgressBar').children('.pbarCaption').remove();
+	$('#powerProgressBar').progressbar({
+		value: pwr
+	}).append("<div class='pbarCaption'>Power</span>");
+	$('#powerProgressBar').prop('title', "" + pwr + " / " + Game.Power);*/
+	$('#powerProgressLabel').text("Power: " + CalculatePowerUsed() + " / " + Game.Power);
+	var amount = CalculatePowerPercentage() * 100.0;
+	$('#powerProgressBar').progressbar({
+		value: amount
+	});
+}
+
+function RegisterPurchaseHook(i) {
+	$('#purchaseButton'+i).click(function(e) {
+		Purchase(i);
+	});
+}
+
+function Purchase(i) {
+	var item = Game.Items[i];
+	if(item.Price <= Game.Money) {
+		RemoveMoney(item.Price);
+		Game.infrastructure[i]++;
+		UpdateInfrastructure();
+	}
+}
